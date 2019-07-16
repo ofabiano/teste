6ebe131d1f7b1580237d09ec2cf9eaef30a01579
ofabiano <ofabiano@gmail.com>
2019-07-16 10:59:42 -0300

    Add files via upload

--- WebApplication_BD/Pages/Index.cshtml.cs
+++ WebApplication_BD/Pages/Index.cshtml.cs
@@ -11,17 +11,22 @@ namespace WebApplication_BD.Pages
     public class IndexModel : PageModel
     {
         public List<string> Messages { get; set; }
+        public string EnvironmentData1 { get; set; }
+        public string EnvironmentData2 { get; set; }
         public void OnGet()
         {
+            EnvironmentData1 = Environment.GetEnvironmentVariable("PATH");
+            EnvironmentData2 = Environment.GetEnvironmentVariable("XPTO");
+
             Messages = new List<string>();
             string connectionString = "Server=CPCLSSQL02\\DEVQA;Database=PortalColeta;User Id=appColeta;Password=appColetadev;";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand("SELECT TOP 10 * FROM TB_FRETE", connection);
-                using(SqlDataReader reader = command.ExecuteReader())
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    while(reader.Read())
+                    while (reader.Read())
                     {
                         Messages.Add(reader["nm_frete"].ToString());
                     }
